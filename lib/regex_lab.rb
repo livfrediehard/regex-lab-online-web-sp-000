13a1438d4c73480bfd3dceb03e313d4004b451bb
Brian Palmer <bpalmer710@gmail.com>
2020-09-18 15:26:15 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,13 +1,13 @@
 def starts_with_a_vowel?(word)
-
+word.scan(/\w+[aeiou]/)
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+word.scan(/\w+un+ing/)
 end
 
 def words_five_letters_long(text)
-
+words.grep(/\w{5}\s/)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
